901f82d734c1736404db438f8ff6f20fde92aa27
MariMarya <maryaeduarda15059927@gmail.com>
2024-03-13 11:39:48 -0300

    segunda pagina

--- MariMarya/MariMaryaPage.xaml.cs
+++ MariMarya/MariMaryaPage.xaml.cs
@@ -1,10 +1,19 @@
+using Microsoft.Maui.Controls;
+
 namespace MariMarya;
 
 public partial class MariMaryaPage : ContentPage
 {
 	public MariMaryaPage()
-{
+	{
 		InitializeComponent();
+	}
 
-} 
+
+	void ButtonVoltar(object sender, EventArgs args)
+	{
+		Application.Current.MainPage =new MainPage();
+	} 
+	
+
 }
